7cc0fbf6b82316a36873ca406c6e701e17142e11
GaemLoader <am0666@inbox.ru>
2023-12-13 15:25:27 +0300

    ss

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        tsk1.task1();
+        task2.task2();
+        task4.task4();
+    }
+}
---
+++ src/tsk1.java
@@ -0,0 +1,13 @@
+import java.util.Date;
+public class tsk1
+{
+    public static void task1()
+    {
+        System.out.println("Фамилия разработчика: Maklakov");
+        Date currentDate = new Date();
+        System.out.println("Дата получения задания: " + currentDate);
+        Date dueDate = new Date(System.currentTimeMillis());
+        System.out.println("Дата окончания: " + dueDate);
+    }
+}
+
